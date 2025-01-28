6a0ca53a578d16ae0af2dde2932be16997392c3e
AigarSem <C21322311@mytudublin.ie>
2025-01-28 11:56:33 +0000

    Progress

---
+++ games-engines-2/boid.gd
@@ -0,0 +1,29 @@
+extends CharacterBody3D
+
+@export var target:Node3D
+@export var force:Vector3
+@export var accel:Vector3
+
+@export var mass:float = 1
+
+var max_speed:float = 10
+
+func seek(target) -> Vector3:
+	var to_target:Vector3 = target.global_position - global_position
+	var desired = to_target.normalized() * max_speed
+	
+	return desired - velocity
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	force = seek(target)
+	
+	accel = force / mass
+	
+	velocity = (velocity + accel * delta)
+	
+	move_and_slide()
